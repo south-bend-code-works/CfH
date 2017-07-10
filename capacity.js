f1f5a883761d961500fe2e2c0a614ed8f29e355e
Shiloh Kelly <shilohjkelly@gmail.com>
2017-07-10 18:59:18 -0400

    Update Capacity.html
    Added Firebase Config

--- capacity.js
+++ capacity.js
@@ -1,7 +1,19 @@
 (function() {
   $(document).ready(init);
-
+  
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBCCOueSDHXoEwdjzR3FDt7hRqhZmqy1No",
+    authDomain: "center-for-the-homeless.firebaseapp.com",
+    databaseURL: "https://center-for-the-homeless.firebaseio.com",
+    projectId: "center-for-the-homeless",
+    storageBucket: "center-for-the-homeless.appspot.com",
+    messagingSenderId: "1019432978985"
+  };
+  firebase.initializeApp(config);
+  
 function init() {
+  firebase.initializeApp(config);
   // alert('it is working');
   $('.bed').click(check);
 }
