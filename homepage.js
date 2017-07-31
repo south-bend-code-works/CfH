0e11f5f7f89f832d03a2226ed47e42458f894590
willow-c02 <wcrisovan@gmail.com>
2017-07-31 19:25:46 -0400

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -1,1 +1,23 @@
-
+(function() {
+  $(document).ready(init);
+  
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBCCOueSDHXoEwdjzR3FDt7hRqhZmqy1No",
+    authDomain: "center-for-the-homeless.firebaseapp.com",
+    databaseURL: "https://center-for-the-homeless.firebaseio.com",
+    projectId: "center-for-the-homeless",
+    storageBucket: "center-for-the-homeless.appspot.com",
+    messagingSenderId: "1019432978985"
+  };
+  firebase.initializeApp(config);
+  
+  
+  function init() {
+    firebase.initializeApp(config);
+    alert('it is working');
+  }
+  
+  
+  
+})();
