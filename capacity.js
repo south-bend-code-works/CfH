d2ea4a002434f8eef7a6e5430b5082dc2ac8a825
Camisa Vines <camisa.vines@icloud.com>
2017-08-21 18:57:24 -0400

    Capacity upload

--- capacity.js
+++ capacity.js
@@ -1,26 +1,13 @@
 (function() {
   $(document).ready(init);
-  
-// Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBCCOueSDHXoEwdjzR3FDt7hRqhZmqy1No",
-    authDomain: "center-for-the-homeless.firebaseapp.com",
-    databaseURL: "https://center-for-the-homeless.firebaseio.com",
-    projectId: "center-for-the-homeless",
-    storageBucket: "center-for-the-homeless.appspot.com",
-    messagingSenderId: "1019432978985"
-  };
-  firebase.initializeApp(config);
-  
+
 function init() {
-  firebase.initializeApp(config);
   // alert('it is working');
   $('.bed').click(check);
 }
 
 function check(){
   $(this).toggleClass("red");
-  // TODO Find bed status in Firebase & show on diagram
 }
 
 })();
