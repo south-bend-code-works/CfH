8877bfc4ea2582f447be4d9496a38bc93824ff88
M. A. Williams <mwilliamsii233203@gmail.com>
2017-09-11 18:40:10 -0400

    Update capacity.js

--- capacity.js
+++ capacity.js
@@ -17,7 +17,7 @@ function init() {
 
 function check(){
   $(this).toggleClass("red");
-      var ref = fireabse.database().ref("Room/Family_1").child("room_1");
+      var ref = fireabse.database().ref("Room");
   ref.once('child_added', function(snapshot){
     var data = snapshot.val();
     console.log(data);
