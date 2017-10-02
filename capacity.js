b3eac2dbc0940eaa5e488cba087c606a10c4822e
Camisa Vines <camisa.vines@icloud.com>
2017-10-02 17:39:15 -0400

    Hopefully the final changes

--- capacity.js
+++ capacity.js
@@ -21,50 +21,49 @@ function init() {
 
 //  These are also alex's suggestions...
 function getBedStatus(){
-  firebase.database().ref().child("Room/Family");
-  firebase.database().ref().child("Room/Family");
-  firebase.database().ref().child("Room/Family");
-  firebase.database().ref().child("Room/Family");
+  firebase.database().ref().child("Room/Family_1");
+  firebase.database().ref().child("Room/Family_2");
+  firebase.database().ref().child("Room/Family_3");
+  firebase.database().ref().child("Room/Family_4");
   firebase.database().ref().child("Room/Men");
   firebase.database().ref().child("Room/Women").once('value', function(snapshot){
     var data = snapshot.val();
     console.log(data);
 
-  //   for(i = 0; i < data.length; i++){
-  //     console.log(data);
-  //   }
-  //
-  //   for(var x in data){
-  //     console.log(data[x]);
-  //   }
-  //   callRoom();
-  //
-  // });
-  // initChangeColor();
+    for(i = 0; i < data.length; i++){
+      console.log(data);
+    }
+
+    for(var x in data){
+      console.log(data[x]);
+    }
+    callRoom();
+
+  });
+  initChangeColor();
 }
 
-// function callRoom(){
-//   var print = snapshot.val(data, 'room_1');
-//   console.log(print);
-// }
-//
-// function initChangeColor() {
-//   console.log('We are here.');
-//   if (firebase.database().ref("Room").val('true')) {
-//     $('.bed').css('background-color', 'red');
-//   } else {
-//     $('.bed').css('background-color', '#032584')
-//   }
-// }
-//
-//
+function callRoom(){
+  var print = snapshot.val(data, 'room_1');
+  console.log(print);
+}
+
+function initChangeColor() {
+  console.log('We are here.');
+  if (firebase.database().ref("Room").val('true')) {
+    $('.bed').css('background-color', 'red');
+  } else {
+    $('.bed').css('background-color', '#032584')
+  }
+}
+
 // function click() {
 //   $('.bed').click(check);
 // }
-//
-// function check(){
-//   $(this).toggleClass("red");
-// }
+
+function check(){
+  $(this).toggleClass("red");
+}
 
 
 
