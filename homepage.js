1d76588a646d9feb2aa511e3a964114aa1eb1868
M. A. Williams <mwilliamsii233203@gmail.com>
2017-10-02 19:12:23 -0400

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,19 +2,17 @@
   $(document).ready(init);
   
 // Initialize Firebase
-  /*var config = {
+  var config = {
     apiKey: "AIzaSyBCCOueSDHXoEwdjzR3FDt7hRqhZmqy1No",
     authDomain: "center-for-the-homeless.firebaseapp.com",
     databaseURL: "https://center-for-the-homeless.firebaseio.com",
     projectId: "center-for-the-homeless",
     storageBucket: "center-for-the-homeless.appspot.com",
     messagingSenderId: "1019432978985"
-  };*/
-  /*firebase.initializeApp(config);*/
-  
+  };
   
   function init() {
-    /*firebase.initializeApp(config);*/
+    firebase.initializeApp(config);
     $('#submit').on("click", getData);
   }
   
