b0803808d99e7c3081b4dc46e61790545d0c97c5
M. A. Williams <mwilliamsii233203@gmail.com>
2017-11-13 18:52:18 -0500

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -22,7 +22,7 @@
       h2.id = "h2";
       h2.textContent = name;
       div.appendChild(h2);
-      $('body').appendChild(div);
+      $('body').append(div);
       $(div).css("display", "fixed");
     });
     //$('#middle').change(function(h2, div){
